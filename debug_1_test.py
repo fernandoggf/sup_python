4a8a726d963ec56779840b0349d56b35136ff145
fernandofig <fernando.ggfigueroa@icloud.com>
2022-02-01 18:09:46 -0600

    test - debug
    test file for the debug code

---
+++ debug_1_test.py
@@ -0,0 +1,30 @@
+from debug_1 import *
+
+# init_script_debug(1, 'https://httpbin.org')
+
+init_script_debug(0, 'https://httpbin.org')
+
+
+car = {
+   "brand": "Ford",
+   "model": "Mustang",
+   "year": "1964"
+ }
+
+# shout('i',car)
+# shout('e',car)
+# shout('q',car)
+# shout('r',car)
+# shout('s',car)
+
+# shout('@',car)
+# shout('%',car)
+# shout('?',car)
+
+# shout('_',car)
+
+# beginDebug('https://httpbin.org')
+
+# endDebug()
+
+# print(obscure('password'))
